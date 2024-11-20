732e7a72ac274949f04b5bb485ab50eaaea4743f
annrisper <annrisperw@gmail.com>
2024-11-20 17:04:13 +0300

    modified

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -55,7 +55,7 @@ export default function LoginForm() {
           </button>
         </form>
         <p className="mt-4 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
+          Don't have an account ?{' '}
           <Link to="/register" className="text-blue-500 hover:text-blue-600">
             Register here
           </Link>
